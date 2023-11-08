c0c6935c7ce99345354d22a6fcae5fffa517c492
Synt4xB4ndit <109498087+Synt4xB4ndit@users.noreply.github.com>
2023-11-07 20:48:44 -0500

    Add files via upload

--- controllers/register.js
+++ controllers/register.js
@@ -29,9 +29,6 @@ const handleRegister = (req, res, db, bcrypt) => {
         .catch(err => res.status(400).json('unable to register'))
 }
 
-const handleSignin = (req, res) => {
-
-}
 
 module.exports = {
     handleRegister: handleRegister
